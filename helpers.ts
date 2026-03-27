80997f1456807a7e0a27e9028cc476cfe83a0354
towfikul <91681146+towfikul@users.noreply.github.com>
2026-03-27 23:48:16 +0600

    Add dark mode support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T17:46:40.115Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-27T17:48:16.236Z
+Commit: Add dark mode support
