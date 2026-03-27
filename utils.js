87e1807e7d5e3289fc0b848be26a4937be9d3579
towfikul <91681146+towfikul@users.noreply.github.com>
2026-03-27 23:48:09 +0600

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-27T17:44:26.849Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-27T17:48:09.308Z
+Commit: Resolve timezone issues in date handling
