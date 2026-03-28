e1170edbf40ac862897ea1c0ab8c4780706bf425
towfikul <91681146+towfikul@users.noreply.github.com>
2026-03-28 21:17:50 +0600

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T15:17:45.050Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-28T15:17:49.442Z
+Commit: Resolve null pointer exception in user service
