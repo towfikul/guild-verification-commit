55e61f87591a3432204f500a4cb360ce7392e7c4
towfikul <91681146+towfikul@users.noreply.github.com>
2026-03-28 21:17:35 +0600

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-28T15:17:30.786Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-28T15:17:35.290Z
+Commit: Fix broken links in documentation
